3a36a526260212aa67db69cc8f97a6e4c24d4e66
Ilnur <92469168+ilnursultan@users.noreply.github.com>
2026-06-24 15:59:01 +0500

    Delete get_data.php

--- get_data.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Разрешаем запросы (на всякий случай)
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=utf-8");
-
-// Просто скачиваем актуальный JSON с твоего гитхаба и отдаем его
-$json = file_get_contents("https://raw.githubusercontent.com/ilnursultan/olotau/main/data.json");
-echo $json;
-?>
